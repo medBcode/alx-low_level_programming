d88d3e0440301fe24b5925645674f6b4c4959fd9
medBcode <medbouagra@gmail.com>
2023-11-22 14:46:08 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
- * main -  prints all arguements
- * @argc: arg count
- * @argv: array of pointers
- * Return: 0
- **/
-
+* main -  prints all arguements
+* @argc: number of arguement , count
+* @argv: the vector that hold arguement
+* Return: 0
+**/
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
-
